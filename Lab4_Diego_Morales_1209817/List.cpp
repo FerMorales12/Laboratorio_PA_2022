12758e5ba7b5eaf9d195aa4c6e7d72b1296a5b9c
DIEGO FERNANDO MORALES RODRIGUEZ <diegofmr0599@hotmail.com>
2022-09-25 21:21:42 -0600

    Nada

--- Lab4_Diego_Morales_1209817/List.cpp
+++ Lab4_Diego_Morales_1209817/List.cpp
@@ -3,5 +3,29 @@
 
 //Insertar al inicio de la lista
 void List::AddStart(int item) {
+	Node* lista = nullptr;
+	Node* nuevoNodo = new Node();
+	nuevoNodo->data = item;
+	Node* aux1 = lista;
+	Node* aux2;
+	
+	//lista ordenada
+	while ((aux1!=nullptr)&&(aux1->data<item))
+	{
+		aux2 = aux1;
+		aux1 = aux1->next;
+	}
+	if (lista == aux1) {
+		lista = nuevoNodo;
+		
+	}
+	else {
+		aux2->next = nuevoNodo;
+		
+	}
+	nuevoNodo->next = aux1;
+}
+
+void List::Insert(int index, int item) {
 
 }
