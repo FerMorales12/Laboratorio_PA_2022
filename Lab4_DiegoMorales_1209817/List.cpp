503643003475b24e21344e64d72eeee2dfb5d730
DIEGO FERNANDO MORALES RODRIGUEZ <diegofmr0599@hotmail.com>
2022-09-30 17:21:15 -0600

    Implementación del método Clear para borrar los elementos de la lista.

--- Lab4_DiegoMorales_1209817/List.cpp
+++ Lab4_DiegoMorales_1209817/List.cpp
@@ -5,20 +5,30 @@
 void List::Add(int item) {
 	Node^ newNode = gcnew Node();
 	newNode->data = item;
-	if (IsEmpty()) {//lo inserta al final de la lista
+	if (Count()==0) {//la lista está vacía 
 		start = newNode;
 		end = newNode;
+	}
+	else {//La lista no está vacía
+		newNode->next = start;
+		start = newNode;
 	}
 	count++;
 	
 }
 
 void List::Clear() {
-
+	Node^ aux = gcnew Node();
+	while (start->next != nullptr) /*mientras la cabeza apunte hacia un elemento, se raliza el while
+		y lo que hace es asignar el valor del siguiente nodo a aux, y eliminar aux*/
+	{start = start->next;
+		start->next = aux;
+		delete &aux;
+	}
 }
 
 int List::Count() {
-
+	return count;
 }
 
 bool List::Contains(int item) {
@@ -34,7 +44,14 @@ void List::Insert(int index, int item) {
 }
 
 int List::GetItem(int index) {
-
+	int counter = 0;
+	Node^ temp = gcnew Node();
+	while ((temp)&& counter <=index)
+	{
+		if (counter < index)
+			return temp->data;
+		counter++;
+	}
 }
 
 void List::SetItem(int index, int item) {
