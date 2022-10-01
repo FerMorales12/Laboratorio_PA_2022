1d75241a30410b53e1a5013233fdae3394499dcb
DIEGO FERNANDO MORALES RODRIGUEZ <diegofmr0599@hotmail.com>
2022-09-30 18:12:53 -0600

    Programando el método Insert. Si la cabeza apunta a null, lo introduce al final de la lista.

--- Lab4_DiegoMorales_1209817/List.cpp
+++ Lab4_DiegoMorales_1209817/List.cpp
@@ -32,38 +32,64 @@ int List::Count() {
 }
 
 bool List::Contains(int item) {
-
+	return false;
 }
 
 int List::IndexOf(int item) {
-
+	return -1;
 }
 
-void List::Insert(int index, int item) {
-
+void List::Insert(int index, int item) {//Inserta en una posición arbitraria en la lista
+	Node^ newNode = gcnew Node();
+	newNode->data = item;
+	if (Count() || index == 0) {
+		Add(item);
+	}
+	else {
+		if (index >= count) {
+			Add(item);
+		}
+		else {
+			Node^ pretemp = gcnew Node();
+			Node^ temp = gcnew Node();
+			int index_intern = 1;
+			while ((temp)&&index_intern<index)
+			{
+				pretemp = temp;
+				temp = temp->next;
+				index_intern++;
+			}
+			newNode->next = temp;
+			pretemp->next = newNode;
+		}
+	}
+	count++;
 }
 
 int List::GetItem(int index) {
 	int counter = 0;
 	Node^ temp = gcnew Node();//el nodo temporal almacenará el dato con el indice n
+	temp->next = start;
 	while ((temp)&& counter <=index)
 	{
 		if (counter < index)
 			return temp->data;
 		counter++;
 	}
+	temp = temp->next;
+	return -1;//Indica que no lo encontró
 }
 
-void List::SetItem(int index, int item) {
+void List::SetItem(int index, int item) {//Si el elemento está en 7, moverlo a 4
 
 }
 
 int List::LastIndexOf(int item) {
-
+	return -1;
 }
 
 bool List::Remove(int item) {
-
+	return true;
 }
 
 void List::RemoveAt(int index) {
