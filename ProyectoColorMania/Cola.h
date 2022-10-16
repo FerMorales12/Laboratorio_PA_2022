f0c69cf62c36e6a75d8d9050dd36e92b4244d586
DIEGO FERNANDO MORALES RODRIGUEZ <diegofmr0599@hotmail.com>
2022-10-15 18:06:35 -0600

    Creación de los métodos para la cola.

--- ProyectoColorMania/Cola.h
+++ ProyectoColorMania/Cola.h
@@ -1,5 +1,15 @@
+#include "Node.h"
 #pragma once
+using namespace System;
 ref class Cola
 {
+	Node^ Head;
+	Node^ Tail;
+	int Length;
+public:
+	void Agregar();
+	void Sacar();
+	void Clear();
+	int getLength();
 };
 
