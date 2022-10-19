01b15fcd9156a04afbb7a191c05ded67f13aa16a
DIEGO FERNANDO MORALES RODRIGUEZ <diegofmr0599@hotmail.com>
2022-10-19 11:47:29 -0600

    Vamos a abrir el archivo inicial para jugar. Creamos un arreglo string para leer la info. del archivo de carga y así poder usar la función split más adelante.

--- ProyectoColorMania/MyForm.h
+++ ProyectoColorMania/MyForm.h
@@ -8,6 +8,7 @@ namespace ProyectoColorMania {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
 
 	/// <summary>
 	/// Resumen de MyForm
@@ -34,6 +35,9 @@ namespace ProyectoColorMania {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
 
 	private:
 		/// <summary>
@@ -48,12 +52,48 @@ namespace ProyectoColorMania {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(345, 27);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(513, 321);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		openFileDialog1->FileName = "";
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+			array<String^>^ lineas = File::ReadAllLines(openFileDialog1->FileName);
+			while (lineas->Length>0)
+			{
+				for (int i = 0; i < lineas->Length; i++) 
+				{
+					array<String^>^ lineas2 = lineas[i]->Split(',');
+				}
+			}
+		}
+	}
 	};
 }
