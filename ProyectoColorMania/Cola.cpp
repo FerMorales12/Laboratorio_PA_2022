15fc8ee158367157e1e3c49798e85e8f96d9fdf8
DIEGO FERNANDO MORALES RODRIGUEZ <diegofmr0599@hotmail.com>
2022-10-20 21:05:35 -0600

    Agregando getItem() a la cola. Para algo debe servir más adelante.

--- ProyectoColorMania/Cola.cpp
+++ ProyectoColorMania/Cola.cpp
@@ -53,11 +53,14 @@ int Cola::getLength() {
 		return largo=0;
 	else {
 		Node^ iterador = gcnew Node();
+		Node^ iteradorcola = gcnew Node();
 		iterador = Head;
+		iteradorcola = Tail;
 		largo = 1;
 		while (iterador->Next!=nullptr)
 		{
 			iterador = iterador->Next;
+			iteradorcola->Next = Tail;
 			largo++;
 		}
 		return largo;
@@ -71,4 +74,16 @@ void Cola::Print() {
 	{
 		actual = actual->Next;
 	}
+}
+
+String^ Cola::getItem(int index) {
+	if (index < 0 || index > this->getLength()) {
+		throw gcnew System::IndexOutOfRangeException();
+	}
+	Node^ iterador = gcnew Node;
+	iterador = Head;
+	for (int i = 0; i < index; i++) {
+		iterador = iterador->Next;
+	}
+	return iterador->Color;
 }
