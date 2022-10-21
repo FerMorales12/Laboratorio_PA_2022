59728b4c77cc9439b0e26226d35b4267a39e1153
farixco <113187191+farixco@users.noreply.github.com>
2022-10-20 19:45:41 -0600

    Definición inicial de MapaDeJuego::Gano

--- ProyectoColorMania/MapadeJuego.cpp
+++ ProyectoColorMania/MapadeJuego.cpp
@@ -17,3 +17,29 @@ void MapaDeJuego::Capacidad(int numCap) {
 	capacidadMaxima = numCap;
 }
 
+bool MapaDeJuego::Gano() {
+	bool gano = true;
+	if (esPila) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < MapaPila[i].Count() - 1; j++) {
+				if (MapaPila[i].GetItem(j) != MapaPila[i].GetItem(j + 1)) {
+					gano = false;
+					i = 5;
+					j = MapaPila[i].Count() + 1;
+				}
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < MapaCola[i].Count() - 1; j++) {
+				if (MapaCola[i].GetItem(j) != MapaCola[i].GetItem(j + 1)) {
+					gano = false;
+					i = 5;
+					j = MapaCola[i].Count() + 1;
+				}
+			}
+		}
+	}
+	return gano;
+}
